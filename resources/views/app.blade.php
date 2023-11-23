b2006750fd682062a97579121bde22f8bed99983
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-11-23 13:43:46 +0100

    Hotjar init

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,17 @@
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        <!-- Hotjar Tracking Code for https://marcoriformato.com/ -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3748496,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
         @inertiaHead
     </head>
     <body class="font-sans antialiased theme-night">
