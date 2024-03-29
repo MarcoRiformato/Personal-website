368b4f40aab333e12f8723f3a027b22838489b3e
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2024-03-29 23:41:45 +0100

    second try for meta title

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppLayout title="Benvenuto">
+  <AppLayout title="Marketing online per studi legali">
 <div class="bg-base overflow-hidden">
 <div class="relative isolate  sm:px-4">
 
@@ -121,8 +121,8 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:mx-0">
         <h2 class="text-base font-semibold leading-7 text-indigo-400">Come funziona?</h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Un percorso dedicato</p>
-        <p class="mt-6 text-lg leading-8 text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis.</p>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Strategia di marketing specializzata per studi legali</p>
+        <p class="mt-6 text-lg leading-8 text-gray-300">Non si tratta della solita agenzie generalista, ma di un percorso dedicato per il marketin online degli studi legali</p>
       </div>
       <dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
         <div v-for="feature in features" :key="feature.name" class="relative pl-9">
@@ -149,10 +149,11 @@
     </div>
 </div>
 
+<!-- Longer description -->
 <div class="bg-gray-900 px-6 py-12 lg:px-8">
     <div class="mx-auto max-w-2xl text-center">
       <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Un modo migliore di fare marketing</h2>
-      <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+      <p class="mt-6 text-lg leading-8 text-gray-300">Con queste statistiche, diventa fondamentale</p>
       <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
       <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
     </div>
