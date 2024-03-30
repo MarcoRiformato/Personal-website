f868bee44c571302c2cf07738f8d92fc87ae66ca
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2024-03-30 19:29:44 +0100

    Half web dev section ready

--- resources/js/Pages/Sviluppo.vue
+++ resources/js/Pages/Sviluppo.vue
@@ -1,85 +1,72 @@
 <template>
 <AppLayout title="Sviluppo">
 
-<!--Sviluppo su misura-->
-<div class="overflow-hidden bg-secondary-content pt-4 py-4">
-<div class="mx-auto max-w-7xl px-6 lg:px-8">
-    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-    <div class="lg:ml-auto lg:pl-4 lg:pt-4">
-        <div class="lg:max-w-lg">
-        <h2 class="text-base font-semibold leading-7 text-secondary">Primo step</h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl text-primary">Un sito web moderno e veloce</p>
-        <p class="mt-6 text-lg leading-8">Creerò il vostro brand distintivo e il posizionamento online, sviluppando in 45 giorni un sito web veloce e mobile-friendly. Post consegna, offrirò aggiornamenti continui per testi e immagini, garantendo al vostro sito dinamicità e attualità.</p>
-        <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
-            <div v-for="primo in primos" :key="primo.name" class="relative pl-9">
-            <dt class="inline font-semibold">
-                <component :is="primo.icon" class="absolute left-1 top-1 h-5 w-5 text-info" aria-hidden="true" />
-                <p class="text-secondary">{{ primo.name }}</p>
-            </dt>
-            {{ ' ' }}
-            <dd class="inline">{{ primo.description }}</dd>
-            </div>
-        </dl>
-        </div>
-    </div>
-    <div class="flex items-center justify-end lg:order-first">
-        <img src="svil0.webp" alt="Product screenshot" class="w-[40rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10" width="2432" height="1442" />
-    </div>
-    </div>
-</div>
-</div>
+<div class="px-6 py-8 lg:px-8">
+  <div class="mx-auto max-w-3xl text-base leading-7">
+    <p class="text-base font-semibold leading-7 text-indigo-600">Primo step</p>
+    <h1 class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Sviluppo web per studi legali</h1>
+    <p class="mt-6 text-xl leading-8">Il sito web del tuo studio legale: è la chiave per un marketing di successo degli studi legali nel 2024. Il tuo sito web costituisce la base di tutte le tue attività di marketing. Investire nel sito giusto è assolutamente cruciale per massimizzare i risultati delle tue campagne di marketing.</p>
+    <div class="mt-10 max-w-2xl">
+      <p>Siamo specializzati in soluzioni di design per siti web di studi legali che ti aiuteranno a far crescere il tuo studio online. Il detto "Non si ha mai una seconda occasione per fare una buona prima impressione" si applica senza dubbio al web. In media, un visitatore si formerà un'opinione sul sito web del tuo studio legale nei primi tre secondi dall'accesso alla tua pagina. Il punto è che se il tuo sito web non cattura immediatamente l'attenzione dei tuoi visitatori, loro se ne andranno per passare al sito web del prossimo avvocato che trovano. Creeremo un sito personalizzato per il tuo studio legale che attirerà l'attenzione dei visitatori e aiuterà a convertire i visitatori del sito in nuovi clienti.</p>
+      <h1 class="text-2xl font-bold mt-4">Cosa rende il nostro servizio di sviluppo diverso?</h1>
+      <ul role="list" class="mt-8 max-w-xl space-y-8">
+        <li class="flex gap-x-3">
+          <CheckCircleIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+          <span><strong class="font-semibold">Ottima prima impressione: </strong> Conosciamo l'importanza di fare una grande prima impressione sui tuoi potenziali clienti. Ecco perché il nostro obiettivo è sempre quello di progettare un sito web che impressioni i visitatori e faccia apparire il tuo studio legale migliore rispetto ai tuoi concorrenti.</span>
+        </li>
+        <li class="flex gap-x-3">
+          <CheckCircleIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+          <span><strong class="font-semibold">Design Personalizzati: </strong>  Tutti i nostri design sono progettati su misura. A differenza della maggior parte delle altre aziende che usano i soliti template tutti uguali, noi creiamo personalmente ogni singolo sito che progettiamo.</span>
+        </li>
+        <li class="flex gap-x-3">
+          <CheckCircleIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+          <span><strong class="font-semibold">Ottimizzati per la SEO: </strong> La maggior parte dei siti web di avvocati esistenti sul web oggi soffrono di un'eccessiva complessità del codice, che solitamente risulta in un impatto negativo sui ranking dei motori di ricerca. Questo perché i siti non sono stati costruiti tenendo in mente la SEO. Noi progettiamo e costruiamo ogni sito mettendo la SEO al primo posto.</span>
+        </li>
+        <li class="flex gap-x-3">
+          <CheckCircleIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+          <span><strong class="font-semibold">Progettati per convertire: </strong>L'obiettivo di ogni sito che creiamo è massimizzare il tasso di conversione dei visitatori del sito. Questo si traduce in ultima analisi in più contatti qualificati e più casi per il tuo studio legale. Utilizziamo ricerca, dati e analisi continue quando si tratta di progettare e costruire siti web per studi legali.</span>
+        </li>
+      </ul>
+      <h2 class="mt-16 text-2xl font-bold tracking-tight text-primary">Come funziona il processo di sviluppo?</h2>
+      <p class="mt-6">Iniziamo ogni progetto di design per siti web di studi legali con un'analisi approfondita del tuo studio e delle sue forze. È importante per noi evidenziare i tuoi punti di forza nel design, quindi è essenziale conoscere e comprendere meglio il tuo studio prima di iniziare il processo di progettazione. Vogliamo sapere cosa ti piace (design, colori, immagini), così come ciò che non ti piace, per assicurarci di creare un sito web che corrisponda alla tua visione.</p>
 
-<!--CTA-->
-<div class="mx-auto max-w-7xl sm:px-6 sm:py-32 lg:px-8 ">
-    <div class="relative isolate overflow-hidden px-6 py-24 bg-primary-content text-center shadow-2xl sm:rounded-3xl sm:px-16">
-    <h2 class="mx-auto max-w-2xl text-3xl font-bold tracking-tight sm:text-4xl text-secondary">Rivoluziona la tua presenza online</h2>
-    <p class="mx-auto mt-6 max-w-xl text-lg leading-8">Dalla definizione del brand allo sviluppo in 45 giorni, siamo i tuoi alleati nel mondo digitale</p>
-    <div class="mt-10 flex items-center justify-center gap-x-6">
-        <button href="#" class="btn btn-primary" @click.prevent="openCalendlyPopup">Prenota consulenza gratuita</button>
+      <p class="mt-6">Dopo aver appreso informazioni sullo studio, avviamo il processo di progettazione. Qui combiniamo le informazioni apprese sul tuo studio con le profonde intuizioni che abbiamo su come i consumatori ricercano e assumono studi legali online. Incorporiamo le migliori pratiche di CRO (Conversion Rate Optimization) in tutti i nostri siti web per massimizzare i contatti che ricevi dai potenziali clienti che visitano il tuo sito web.</p>
+
+      <p class="mt-6">Successivamente, ti presentiamo il design che abbiamo creato. Lo esamineremo insieme e, se necessario, apporteremo revisioni al design per perfezionarlo. Una volta ottenuto il tuo sigillo di approvazione, possiamo iniziare il processo di sviluppo, che dà vita al tuo sito web. Dopo che il lavoro di sviluppo web è stato completato, sarai in grado di rivedere il sito web pienamente funzionante in un ambiente di sviluppo prima di pubblicare il tuo nuovo sito sul web.</p>
+
+      <h2 class="mt-16 text-2xl font-bold tracking-tight text-primary">Siti web per studi legali che ti aiutano a generare più entrate</h2>
+
+      <p class="mt-6">Crediamo in un marketing per studi legali più intelligente. Le campagne di marketing di successo per gli studi legali iniziano sempre con siti web o pagine di atterraggio più intelligenti. Sappiamo che i visitatori del tuo sito spesso confrontano il tuo sito web con quelli di altri studi legali, quindi il nostro obiettivo è creare un sito web che sia visivamente e funzionalmente superiore a quelli dei tuoi concorrenti. In ultima analisi, questi fattori possono aiutarti a convertire un maggior numero di visitatori del sito in clienti effettivamente acquisiti.</p>
+
+      <p class="mt-6">Se l'obiettivo del tuo sito è quello di generare più casi studio, è ancora più importante avere un sito web costruito in modo ottimizzato per i motori di ricerca. Dato che la nostra competenza è nella SEO, tutti i nostri siti web sono programmati con codice che permette a tutto il contenuto sul tuo sito web di essere trovato dai motori di ricerca. Sviluppiamo siti web ottimizzati per la SEO per garantire che il tuo studio possa massimizzare la sua visibilità su Google. Una migliore visibilità online significa più contatti qualificati per il tuo studio legale.</p>
+      
+      <figure class="mt-10 border-l border-indigo-600 pl-9">
+        <blockquote class="font-semibold">
+          <p>Il tuo sito web è la base di una campagna di marketing oline di successo. Investire in un sito di alta qualità, costruito professionalmente, significa ottenere un tasso di conversione più alto dei visitatori del sito, il che alla fine significa più affari per il tuo studio legale.</p>
+        </blockquote>
+      </figure>
     </div>
-    <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]" aria-hidden="true">
-        <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7" />
-        <defs>
-        <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
-            <stop stop-color="#7775D6" />
-            <stop offset="1" stop-color="#E935C1" />
-        </radialGradient>
-        </defs>
-    </svg>
-    </div>
+    <figure class="mt-16">
+      <img class="aspect-video rounded-xl bg-gray-50 object-cover" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="" />
+      <figcaption class="mt-4 flex gap-x-2 text-sm leading-6">
+        <InformationCircleIcon class="mt-0.5 h-5 w-5 flex-none" aria-hidden="true" />
+        Faucibus commodo massa rhoncus, volutpat.
+      </figcaption>
+    </figure>
+  </div>
 </div>
 
 </AppLayout>
 </template>
 
 <script setup>
+import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/vue/20/solid'
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon, LightBulbIcon, CodeBracketIcon, ArrowPathIcon,
-  ClipboardDocumentIcon, PencilSquareIcon, CheckCircleIcon
-
-} from '@heroicons/vue/20/solid'
 
 function openCalendlyPopup(event) {
 event.preventDefault();
 Calendly.initPopupWidget({ url: 'https://calendly.com/marco-riformato/incontro-di-valutazione' });
 }
 
-const primos = [
-  {
-    name: 'Ideazione brand',
-    description:
-      'Avremo un colloquio conoscitivo durante il quale stabiliremo le priorità e gli obbiettivi da raggiungere. Sarà mia cura ideare un brand e pianificare il posizionamento della tua azienda online. ',
-    icon: LightBulbIcon,
-  },
-  {
-    name: 'Sviluppo',
-    description: 'In 45 giorni, svilupperemo un sito web su misura. Avremo cura di renderlo veloce e ottimizzato per i dispositivi mobili, che oggi costituiscono oltre il 50% della navigazione su internet',
-    icon: CodeBracketIcon,
-  },
-  {
-    name: 'Consegna ed evoluzione continua',
-    description: 'Una volta terminato lo sviluppo ed avvenuta la consegna, sarò sempre a tua disposizione per ulteriori modifiche. Eseguirò gratuitamente qualunque richiesta di modifica al sito che riguardi un testo od un immagine.',
-    icon: ArrowPathIcon,
-  },
-]
+
 </script>
