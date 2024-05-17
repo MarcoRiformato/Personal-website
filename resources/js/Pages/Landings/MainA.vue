c561a6261e77a77afe16c6fd291139b771a40f9d
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2024-05-17 12:24:43 +0200

    fix "marketin" typo

--- resources/js/Pages/Landings/MainA.vue
+++ resources/js/Pages/Landings/MainA.vue
@@ -116,7 +116,7 @@
       <div class="mx-auto max-w-2xl lg:mx-0">
         <h2 class="text-base font-semibold leading-7 text-indigo-400">Come funziona?</h2>
         <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Strategia di marketing specializzata per studi legali</p>
-        <p class="mt-6 text-lg leading-8 text-gray-300">Non si tratta della solita agenzie generalista, ma di un percorso dedicato per il marketin online degli studi legali</p>
+        <p class="mt-6 text-lg leading-8 text-gray-300">Non si tratta della solita agenzie generalista, ma di un percorso dedicato per il marketing online degli studi legali</p>
       </div>
       <dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
         <div v-for="feature in features" :key="feature.name" class="relative pl-9">
